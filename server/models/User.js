6330713bfd3e8092a87f9d4dbeab4d0d8c58d750
Aayush-207 <aayushraut2006@gmail.com>
2025-04-19 11:11:34 +0530

    -

--- server/models/User.js
+++ server/models/User.js
@@ -20,7 +20,36 @@ const User = mongoose.model('User', userSchema);
 
 module.exports = User;
 
+const data = new mongoose.Schema({
+    q1: {
+        type: String,
+        required: true,
+    },
+    q2: {
+        type: String,
+        required: true,
+    },
+    q3: {
+        type: String,
+        required: true,
+    },
+    q4: {
+        type: String,
+        required: true,
+    },
+    q5: {
+        type: String,
+        required: true,
+    },
+    q6: {
+        type: String,
+        required: true,
+    }
+});
 
+const Data = mongoose.model('Data', data);
+
+module.exports = Data;
 
 
 
